0b69f4273da79e28a5430adf1deca1378b93c7c6
Smiley <jcsmileyjr@gmail.com>
2022-11-01 13:18:20 -0500

    Feature: Dummy data for the list of Todos

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,19 +1,36 @@
+import { React, useState, useEffect } from "react";
+import dummyData from "./development-data/dummyData.json";
 import "./App.css";
-import AddTodo from './components/addTodo/AddTodo.js';
+import AddTodo from "./components/addTodo/AddTodo.js";
 import TodoItem from "./components/todo/Todo.js";
 
 function App() {
+  const [todoList, setTodoList] = useState([]); // Current Todo items displayed
+
+  // Update the application, upon start, with the list of Todos
+  useEffect(() => {
+    getTodo();
+  }, []);
+
+  // Method to update the application state with the current list of Todos
+  const getTodo = () => {
+    const currentTodoList = dummyData;
+    setTodoList(currentTodoList);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>TODO</h1>
       </header>
       <main>
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <AddTodo />
-        
+        {/*Display Todo items */}
+        {todoList.map((todo) => (
+          <TodoItem key={todo.id} />
+        ))}
+
+        {/*Display button to add a new todo */}
+        <AddTodo />
       </main>
     </div>
   );
