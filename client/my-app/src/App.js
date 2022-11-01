14d2298876384e65bbd7fcce030db71ae53e62be
Smiley <jcsmileyjr@gmail.com>
2022-11-01 14:41:50 -0500

    Feature: Pop modal UI to create a new todo

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,11 +1,25 @@
+import Modal from "react-modal";
 import { React, useState, useEffect } from "react";
 import dummyData from "./development-data/dummyData.json";
 import "./App.css";
 import AddTodo from "./components/addTodo/AddTodo.js";
 import TodoItem from "./components/todo/Todo.js";
 
+const customStyles = {
+  content: {
+    top: "25%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "70%",
+  },
+};
+
 function App() {
   const [todoList, setTodoList] = useState([]); // Current Todo items displayed
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   // Update the application, upon start, with the list of Todos
   useEffect(() => {
@@ -16,6 +30,16 @@ function App() {
   const getTodo = () => {
     const currentTodoList = dummyData;
     setTodoList(currentTodoList);
+  };
+
+  const openModal = () => {
+    console.log("open model");
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    console.log("close modal");
+    setIsOpen(false);
   };
 
   return (
@@ -30,8 +54,25 @@ function App() {
         ))}
 
         {/*Display button to add a new todo */}
-        <AddTodo />
+        <AddTodo open={() => openModal()} />
       </main>
+
+      {/*Pop up modal to create a new TODO item */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Pop up modal to create a new TODO task"
+        ariaHideApp={false}
+      >
+        <h2 className="modal__title--style">Type in a new Todo</h2>
+        <div className="modal__button--container">
+          <input className="modal__input--style" type="text" name="todoInput" />
+          <button className="card__button--style" onClick={closeModal}>
+            close
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
--- client/my-app/src/components/addTodo/AddTodo.js
+++ client/my-app/src/components/addTodo/AddTodo.js
@@ -1,10 +1,10 @@
 import './addTodo.css'
 import AddImage from "../../assets/add.png";
 
-const AddTodo = () => {
+const AddTodo = ({open}) => {
   return (
     <>
-      <button name="addTodo" className="addTodo__button--style" type="button">
+      <button onClick={open} name="addTodo" className="addTodo__button--style" type="button">
         <img className="addTodo__image--style" src={AddImage} alt="" />
         Add a new TODO item
       </button>
