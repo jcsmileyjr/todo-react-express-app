94a8c53c33a3443f4f6960c298acf87d93f6ca31
Smiley <jcsmileyjr@gmail.com>
2022-11-05 11:08:27 -0500

    Refactor show checked input fields based on TODO status

--- client/my-app/src/components/todo/Todo.js
+++ client/my-app/src/components/todo/Todo.js
@@ -1,13 +1,12 @@
-import {React, useState} from 'react';
+import {React} from 'react';
 import "./todo.css"
 import DeleteImage from "../../assets/delete.png";
 
 const Todo = ({updateTodo, deleteTodo, todo}) => {
-    const [checked, setChecked] = useState(false);
     return(
         <div className="todo--container">
           <div className="todo__content--container">
-            <input onChange={() => updateTodo(todo.id)} className="todo__checkbox--style" type="checkbox" id="1" name="todoCheckbox" value={todo.content} />
+            <input onChange={() => updateTodo(todo.id)} checked={todo.status==='in-progress'?false:true} className="todo__checkbox--style" type="checkbox" id="1" name="todoCheckbox" value={todo.content} />
             <label htmlFor="todoCheckbox"> {todo.content}</label>
           </div>
           <img onClick={() => deleteTodo(todo.id)} className="todo__deleteImage--style" src={DeleteImage} alt="" />
